9fb5998c9c3e3e7903eea878520ce4510050f88b
Lasse Henriksen <lass5676@edu.easj.dk>
2019-03-27 11:29:57 +0100

    Tilføjet en virkende "ShowAllCars" function

---
+++ src/js/index.ts
@@ -0,0 +1,17 @@
+import axios,{
+    AxiosResponse, AxiosError
+} from "../../node_modules/axios/index"
+import { ICar } from "./Icar";
+let contentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
+function showAllCars():void {
+    axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+    .then(function(response:AxiosResponse<ICar[]>): void {
+        console.log(response);
+    })
+    .catch(function(error : AxiosError) : void {
+        console.log(error);
+    })
+    
+}
+showAllCars();
